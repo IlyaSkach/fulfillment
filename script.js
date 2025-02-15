b357fa70dc72ba1c225321c0277bc537be8a4e8a
Илья С <skachilya@gmail.com>
2025-02-15 23:16:38 +0200

    how sec v1

--- script.js
+++ script.js
@@ -30,4 +30,37 @@ document.querySelectorAll('.services-item-card').forEach(card => {
   });
 });
 
+gsap.registerPlugin(ScrollTrigger);
+
+let mainAnimation = gsap.to("#movingSvg", {
+    x: "100vw", // Двигаем вправо
+    duration: 15, // Делаем движение медленнее
+    ease: "power1.inOut", // Плавное начало и конец
+    yoyo: false, // Не повторяем назад
+    repeat: -1, // Зацикливаем анимацию
+    paused: true, // Начинаем анимацию в паузе
+    onUpdate: function() {
+        let progress = this.progress(); // Получаем текущий прогресс анимации (от 0 до 1)
+        if (Math.random() < 0.003) { // 0.3% вероятность подпрыгивания
+            gsap.to("#movingSvg", { 
+                y: -10, // Подпрыгивание вверх
+                duration: 0.2, 
+                ease: "power1.out", 
+                yoyo: true, 
+                repeat: 1, 
+                onComplete: () => { gsap.to("#movingSvg", { y: 0, duration: 0.2, ease: "power1.inOut" }); } // Плавный возврат на линию
+            });
+        }
+    }
+});
+
+ScrollTrigger.create({
+    trigger: "#how", // Элемент, который должен быть видимым для запуска анимации
+    start: "top center", // Начало триггера (когда верхняя часть элемента достигает центра области просмотра)
+    onEnter: () => mainAnimation.play(), // Запуск анимации при входе в область просмотра
+    onLeave: () => mainAnimation.pause(), // Пауза анимации при выходе из области просмотра
+    onEnterBack: () => mainAnimation.play(), // Возобновление анимации при возврате в область просмотра
+    onLeaveBack: () => mainAnimation.pause() // Пауза анимации при выходе из области просмотра назад
+});
+
 
